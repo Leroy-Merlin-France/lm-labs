d087a8ba0514bf8913bf053bc46431acdc57628f
Vincent Dutat <vdutat@yahoo.fr>
2013-02-25 14:20:46 +0100

    LMLAB-196 Flux rss actu - afficher miniature https://archidevlm.atlassian.net/browse/LMLAB-196

--- lm-labs-core/src/main/java/com/leroymerlin/corp/fr/nuxeo/labs/site/news/NewsTools.java
+++ lm-labs-core/src/main/java/com/leroymerlin/corp/fr/nuxeo/labs/site/news/NewsTools.java
@@ -1,12 +1,42 @@
 package com.leroymerlin.corp.fr.nuxeo.labs.site.news;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.ClientException;
 
 import com.sun.syndication.feed.synd.SyndContent;
 import com.sun.syndication.feed.synd.SyndContentImpl;
+import com.sun.syndication.feed.synd.SyndEnclosure;
+import com.sun.syndication.feed.synd.SyndEnclosureImpl;
 
 public class NewsTools {
+
+    private static final Log LOG = LogFactory.getLog(NewsTools.class);
+    
+    private NewsTools () {}
+    
+    public static List<SyndEnclosure> createRssNewsEnclosures(final LabsNews news, final String url) throws ClientException {
+        List<SyndEnclosure> enclosures = new ArrayList<SyndEnclosure>();
+        if (news.hasSummaryPicture()) {
+            SyndEnclosure enclosure = new SyndEnclosureImpl();
+            try {
+                Blob picture = news.getSummaryPicture();
+                enclosure.setType(picture.getMimeType());
+                enclosure.setUrl(url + "/summaryPictureTruncated");
+                enclosure.setLength(picture.getLength());
+                enclosures.add(enclosure);
+            } catch (IOException e) {
+                LOG.error(e, e);
+            }
+        }
+        return enclosures;
+    }
     
     public static SyndContent createRssNewsDescription(LabsNews news) throws ClientException {
         SyndContent description;
