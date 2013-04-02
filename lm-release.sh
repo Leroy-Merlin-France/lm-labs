62284cf27f58067efb7ce97ac49b2ff7354da4a4
Vincent Dutat <vdutat@yahoo.fr>
2013-04-02 11:43:00 +0200

    move lm-release.sh to git

--- lm-release.sh
+++ lm-release.sh
@@ -23,28 +23,44 @@ make_branch() {
     echo "============> Finding pom(s).xml       <=================================="
     poms=`find . -name pom.xml`
 
-    hg up develop
+    git checkout master
+    git checkout develop
 
     #== Passer à la version release (nouvelle branche)==
+
+    git checkout -b release-$RELEASE_VERSION develop
 
     echo "============> Creating branch $RELEASE_VERSION <=========================="
     for pom in $poms; do $SED "/<parent>/,/<\/parent>/ s,<version>$CURRENT_SNAPSHOT<,<version>$RELEASE_VERSION<," $pom; done
     $SEDE "/<project/,/<properties>/ s,<version>$CURRENT_SNAPSHOT</version>,<version>$RELEASE_VERSION</version>," pom.xml
 
-    hg branch release-$RELEASE_VERSION
-    hg ci -m "Prepare release $RELEASE_VERSION"
+    git commit -a -m "Prepare release $RELEASE_VERSION"
+
+    echo "============> Merging $CURRENT_VERSION branch into master branch <===================="
+
+    git checkout master
+    git merge --no-ff release-$RELEASE_VERSION
+    git tag $RELEASE_VERSION
 
     echo "============> Moving back to $CURRENT_VERSION branch <===================="
-    hg up develop
+    git checkout develop
 
     # == Préparer la branche snapshot suivante ==
+
+    git merge --no-ff release-$RELEASE_VERSION
+
     if [ "x" != "x$3" ]; then
         echo "============> Creating new Snapshot branch $CURRENT_SNAPSHOT <============"
         for pom in $poms; do $SED "/<parent>/,/<\/parent>/ s,<version>$CURRENT_SNAPSHOT<,<version>$NEXT_SNAPSHOT<," $pom; done
         $SEDE "/<project/,/<properties>/ s,<version>$CURRENT_SNAPSHOT</version>,<version>$NEXT_SNAPSHOT</version>," pom.xml
-        hg ci -m "Building new $NEXT_VERSION "
+        git commit -a -m "Building new $NEXT_VERSION "
 	    
     fi
+
+    echo "NOW YOU NEED TO PUSH ALL BRANCHES, SUBMIT THE FOLLOWING COMMANDS :"
+    echo "git push"
+    echo "git checkout release-$RELEASE_VERSION"
+    echo "git push origin release-$RELEASE_VERSION"
 }
 
 make_branch $@
