e60a108c209d1dde4cb113c5f82412f31b54d7ec
Angélique Muller <angelique.muller@ineat-conseil.fr>
2014-03-06 16:39:30 +0100

    Prepare release 2.22.0

--- lm-release.sh
+++ lm-release.sh
@@ -28,13 +28,13 @@ make_branch() {
     echo "============> Finding pom(s).xml       <=================================="
     poms=`find . -name pom.xml`
 
-    git checkout master
-    git pull
-    git checkout develop
+    #git checkout master
+    #git pull
+    #git checkout develop
 
     #== Passer à la version release (nouvelle branche)==
 
-    git checkout -b release-$RELEASE_VERSION develop
+    #git checkout -b release-$RELEASE_VERSION develop
 
     echo "============> Creating branch $RELEASE_VERSION <=========================="
     for pom in $poms; do $SED "/<parent>/,/<\/parent>/ s,<version>$CURRENT_SNAPSHOT<,<version>$RELEASE_VERSION<," $pom; done
@@ -44,24 +44,24 @@ make_branch() {
 
     echo "============> Merging $CURRENT_VERSION branch into master branch <===================="
 
-    git checkout master
-    git merge --no-ff release-$RELEASE_VERSION
-    git tag $RELEASE_VERSION
+    #git checkout master
+    #git merge --no-ff release-$RELEASE_VERSION
+    #git tag $RELEASE_VERSION
 
     echo "============> Moving back to $CURRENT_VERSION branch <===================="
-    git checkout develop
+    #git checkout develop
 
     # == Préparer la branche snapshot suivante ==
 
-    git merge --no-ff release-$RELEASE_VERSION
+    #git merge --no-ff release-$RELEASE_VERSION
 
-    if [ "x" != "x$3" ]; then
-        echo "============> Creating new Snapshot branch $NEXT_SNAPSHOT <============"
-        for pom in $poms; do $SED "/<parent>/,/<\/parent>/ s,<version>$RELEASE_VERSION<,<version>$NEXT_SNAPSHOT<," $pom; done
-        $SEDE "/<project/,/<properties>/ s,<version>$RELEASE_VERSION</version>,<version>$NEXT_SNAPSHOT</version>," pom.xml
-        git commit -a -m "Building new $NEXT_VERSION "
+    #if [ "x" != "x$3" ]; then
+    #    echo "============> Creating new Snapshot branch $NEXT_SNAPSHOT <============"
+    #    for pom in $poms; do $SED "/<parent>/,/<\/parent>/ s,<version>$RELEASE_VERSION<,<version>$NEXT_SNAPSHOT<," $pom; done
+    #    $SEDE "/<project/,/<properties>/ s,<version>$RELEASE_VERSION</version>,<version>$NEXT_SNAPSHOT</version>," pom.xml
+    #    git commit -a -m "Building new $NEXT_VERSION "
 	    
-    fi
+    #fi
 
     echo "NOW YOU NEED TO PUSH ALL BRANCHES, SUBMIT THE FOLLOWING COMMANDS :"
     echo "git push"
